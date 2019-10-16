337efc257a0095800362d8ae418ecc5b39d11065
段体华 <duantihua@gmail.com>
2019-10-16 17:29:25 +0800

    fix #1 在注入bean时支持Option类型

--- spring/src/test/scala/org/beangle/cdi/spring/bean/OptionDao.scala
+++ spring/src/test/scala/org/beangle/cdi/spring/bean/OptionDao.scala
@@ -16,17 +16,8 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.beangle.cdi.spring.beans
+package org.beangle.cdi.spring.bean
 
-import java.beans.PropertyEditorSupport
+class OptionDao {
 
-class OptionEditor extends PropertyEditorSupport {
-
-  override def setAsText(text: String): Unit = {
-    setValue(text)
-  }
-
-  override def setValue(value: AnyRef): Unit = {
-    super.setValue(Option(value))
-  }
 }
